84a13e4ee0ef398efc4609fee5ad24d080265fbf
SIPHUMELELE22 <siphumelelesiphosethu22@gmail.com>
2023-10-26 22:33:03 +0200

    [Save and Refresh] Update project with latest changes
    In this commit, I have saved all my current work and refreshed the project to ensure it reflects the most recent changes from the Git repository. This helps keep my local workspace up to date and in sync with the latest code updates. No functional code changes were made in this commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: AuthGuard
   },
   {
     path: '',
@@ -21,7 +22,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'gallery',
@@ -43,6 +44,10 @@ const routes: Routes = [
     path: 'news',
     loadChildren: () => import('./pages/news/news.module').then( m => m.NewsPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  }
 ];
 
 @NgModule({
